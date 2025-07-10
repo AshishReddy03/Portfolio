0e2f31835e03b77e27daabfb585eab3f0b7ef78c
Ashish Reddy <ashishreddy@Ashishs-MacBook-Air.local>
2025-07-10 12:09:01 +0530

    Refactored Tailwind setup using @tailwindcss/vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  theme: {
-    extend: {
-      fontFamily: {
-        mono: ['"Fira Code"', "monospace"],
-      },
-    },
-  },
 });
